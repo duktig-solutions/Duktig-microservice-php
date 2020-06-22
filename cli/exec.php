5299738ce5876ba69752469c7003c1f551dbd53a
tokernel <tokernel@gmail.com>
2020-06-22 13:21:45 +0400

    Initial commits

---
+++ cli/exec.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+/**
+ * Command Line Interface.
+ *
+ * @author David A. <software@duktig.dev>
+ * @license see License.md
+ * @version 1.0.0
+ */
+namespace System;
+
+# Define project root path
+define('DUKTIG_ENV', 'cli');
+define('DUKTIG_ROOT_PATH', realpath(__DIR__ . '/../') . '/');
+define('DUKTIG_APP_PATH', realpath(DUKTIG_ROOT_PATH . 'app') . '/');
+
+# Include Constants file
+require_once (DUKTIG_APP_PATH . 'config/constants.php');
+
+# Include Autoloader
+require_once (DUKTIG_ROOT_PATH . 'vendor/autoload.php');
+
+/**
+ * Because this is command line interface,
+ * the process can run without time limitation.
+ */
+set_time_limit(0);
+
+# Set Configuration
+ini_set('log_errors', (string) Config::get()['LogErrors']);
+ini_set('display_errors', (string) Config::get()['DisplayErrors']);
+ini_set('date.timezone', (string) Config::get()['DateTimezone']);
+
+# Initialize Input/Output
+$input = new Input($argv);
+$output = new Output();
+
+# Set error handler
+set_error_handler(function($code, $message, $file, $line) use ($output) {
+
+    # This will return true, if not notice
+    # In case if this is not a notice, we throwing Exception
+    if(Ehandler::processError($message, $code, $file, $line)) {
+        $output->stderr("\nError! " . $message);
+    }
+
+});
+
+try {
+
+    # Check if the service is under maintenance
+    if(Config::get()['DisableCLI'] == 1) {
+
+        $output->stdout('CLI access is rejected due to the disabled flag. See /app/config/app.php - DisableCLI.');
+        Logger::Log('CLI access is rejected due to the disabled flag. See /app/config/app.php - DisableCLI', Logger::WARNING, __FILE__, __LINE__);
+
+        exit();
+
+    }
+
+    # Initialize Route
+    # If no route match/found to run, Routing will automatically exit with error
+    CliRouter::init($input, $output);
+
+} catch(\Throwable $e) {
+
+    \System\Ehandler::processError($e->getMessage(), 0, $e->getFile(), $e->getLine());
+
+	# In some cases, the process can be locked.
+	# Let's try to unlock
+	CliRouter::unlockProcess();
+
+    $output->stderr($e->getMessage());
+
+}
---
+++ www/index.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1); 
+/**
+ * HTTP entrypoint for Web Servers such as Apache, Nginx.
+ * To setup environment with web server you have to set entrypoints/http/ as web public directory.
+ *
+ * @author David A. <software@duktig.dev>
+ * @license see License.md
+ * @version 1.0.0
+ */
+namespace System;
+
+# Define project root path
+define('DUKTIG_ENV', 'http');
+define('DUKTIG_ROOT_PATH', realpath(__DIR__ . '/../') . '/');
+define('DUKTIG_APP_PATH', realpath(DUKTIG_ROOT_PATH . 'app') . '/');
+
+# Include Constants file
+require_once (DUKTIG_APP_PATH . 'config/constants.php');
+
+# Include Autoloader
+require_once (DUKTIG_ROOT_PATH . 'vendor/autoload.php');
+
+# Initialize Request/Response
+$request = new Request();
+$response = new Response();
+
+# Set error handler
+set_error_handler(function($code, $message, $file, $line) use($response) {
+
+    # This will return true, if not notice
+    # In case if this is not a notice, we throwing Exception
+    if(Ehandler::processError($message, $code, $file, $line)) {
+
+        # Reset Response data, Set new data and output.
+        $response->reset();
+        $response->sendJson(
+            Ehandler::getDetailed($message, $code),
+            500
+        );
+        $response->sendFinal();
+    }
+
+});
+
+try {
+
+	ob_start();
+
+	# Set Configuration
+	ini_set('log_errors', (string) Config::get()['LogErrors']);
+	ini_set('display_errors', (string) Config::get()['DisplayErrors']);
+	ini_set('date.timezone', (string) Config::get()['DateTimezone']);
+
+	# Check if the service is under maintenance
+	if(Config::get()['UnderMaintenance'] == 1) {
+			
+		$response->sendJson([
+			'status' => 'ok',
+			'message' => 'Under maintenance'
+		], 503);
+		
+	}
+
+	# Initialize Route
+	# If no route match/found to run, Routing will automatically send Error 404
+	HttpRouter::init($request, $response);
+
+	# Finally send response to client.
+    $response->sendFinal();
+
+} catch(\Throwable $e) {
+
+    Ehandler::processError($e->getMessage(), 0, $e->getFile(), $e->getLine());
+
+    # Reset Response data, Set new data and output.
+    $response->reset();
+    $response->sendJson(
+        Ehandler::getDetailed($e->getMessage(), $e->getCode()),
+        500
+    );
+    $response->sendFinal();
+
+}
